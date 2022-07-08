8032224982dd401e0302c0bb7d7954cc5f035666
Daniel <danielcvt@gmail.com>
2022-07-08 00:08:08 -0300

    improve folder structure, save caption as txt.

--- src/download-media.ts
+++ src/download-media.ts
@@ -4,7 +4,27 @@ import https from 'https'
 import { writeJSON } from './writefile'; './writefile'
 
 export async function downloadMedia(username: string){
-
+  
+  function checkExistentFile(filename: string, username: string, path: string){
+    if (fs.readdirSync(`./output/${username}${path}`).findIndex(f => f.startsWith(filename))==-1){
+      return false
+    }
+    return true
+  }
+  
+  function downloadMedia(mediaURL: string, extension: string, filename: string, username: string, savingDir: string) {
+    https.get(mediaURL, (res) => {
+      const path = `./output/${username}${savingDir}${filename}.${extension}`;
+      const writeStream = fs.createWriteStream(path);
+      
+      res.pipe(writeStream);
+      
+      writeStream.on("finish", () => {
+        writeStream.close();
+        console.log(filename+'.'+extension+" Download Completed");
+      });
+    });
+  }
   let allPosts: UserFeedResponseItemsItem[] = [];
   
   
@@ -16,30 +36,41 @@ export async function downloadMedia(username: string){
   console.log(files[fileToRead])
   let data = fs.readFileSync(`./output/${username}/${options[fileToRead]}`)
   fs.mkdirSync(`./output/${username}/posts`, { recursive: true })
-  fs.mkdirSync(`./output/${username}/posts/media`, { recursive: true })
   
   let posts: UserFeedResponseItemsItem[] = JSON.parse(data.toString())
   posts.map(post => {
-    let filename = new Date(post.taken_at*1000).toISOString()
-    if (!checkExistentFile(filename, username,'/posts')){
-      writeJSON(JSON.stringify(post),username,'/posts/'+ filename)
+    let postDate = new Date(post.taken_at*1000)
+    let filename = postDate.toISOString()
+    let foldername = postDate.toISOString().slice(0,10) +"-"+ post.pk.toString()
+    let savingDir = `/posts/${foldername}/`
+    
+    fs.mkdirSync(`./output/${username}/posts/${foldername}`, { recursive: true })
+    if (!checkExistentFile(filename, username,savingDir)){
+      writeJSON(JSON.stringify(post),username,savingDir+ foldername)
       
     }
+    
+
+    if(post.caption){
+      fs.writeFileSync(`./output/${username}/posts/${foldername}/caption.txt`, post.caption.text, 'utf8')
+    }
+
+
     let extension = ''
     let mediaURL = ''
     switch (post.media_type) {
       case 2:
         mediaURL = post.video_versions![0].url
         extension = 'mp4'
-        if (!checkExistentFile(post.pk, username, '/posts/media')){
-          downloadMedia(mediaURL, extension, post.pk, username)
+        if (!checkExistentFile(post.pk, username, savingDir)){
+          downloadMedia(mediaURL, extension, post.pk, username, savingDir)
         }
         break;
         case 1:
           mediaURL = post.image_versions2.candidates[0].url
           extension = 'jpg'  
-          if (!checkExistentFile(post.pk, username, '/posts/media')){
-            downloadMedia(mediaURL, extension, post.pk, username)
+          if (!checkExistentFile(post.pk, username, savingDir)){
+            downloadMedia(mediaURL, extension, post.pk, username, savingDir)
           }
           break;
           case 8:
@@ -50,14 +81,14 @@ export async function downloadMedia(username: string){
           case 1:
             mediaURL = media.image_versions2.candidates[0].url
             extension = 'jpg'
-            if (!checkExistentFile(post.pk+'-'+index, username, '/posts/media'))
-            downloadMedia(mediaURL, extension, post.pk+'-'+index, username)
+            if (!checkExistentFile(post.pk+'-'+index, username, savingDir))
+            downloadMedia(mediaURL, extension, post.pk+'-'+index, username, savingDir)
             break;
             case 2:
               mediaURL = media.video_versions![0].url;
               extension = 'mp4'
-              if (!checkExistentFile(post.pk+'-'+index, username, '/posts/media'))
-              downloadMedia(mediaURL, extension, post.pk+'-'+index, username)
+              if (!checkExistentFile(post.pk+'-'+index, username, savingDir))
+              downloadMedia(mediaURL, extension, post.pk+'-'+index, username, savingDir)
               break
               default:
                 break;
@@ -67,26 +98,7 @@ export async function downloadMedia(username: string){
             break;
             default:
               break;
-            }
-            function checkExistentFile(filename: string, username: string, path: string){
-              if (fs.readdirSync(`./output/${username}${path}`).findIndex(f => f.startsWith(filename))==-1){
-                return false
-              }
-              return true
-            }
-            
-            function downloadMedia(mediaURL: string, extension: string, filename: string, username: string) {https.get(mediaURL, (res) => {
-              const path = `./output/${username}/posts/media/${filename}.${extension}`;
-              const writeStream = fs.createWriteStream(path);
-              
-              res.pipe(writeStream);
-              
-              writeStream.on("finish", () => {
-                writeStream.close();
-                console.log(filename+'.'+extension+" Download Completed");
-              });
-  });
-  }
+    }
 
 })
 }
